566fa8c3b3452d9382e7c31a4e15d31d7c5d7446
Mohak <mohak.agarwal_ccv17@gla.ac.in>
2019-02-20 11:21:19 +0530

    added constructors

--- src/main/Node.java
+++ src/main/Node.java
@@ -15,4 +15,26 @@ public class Node {
     public void setData(int data) {
         this.data = data;
     }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node() {
+        this.data = 0;
+        this.next = null;
+    }
+
+    public Node(Node next) {
+        this.next = next;
+    }
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
 }
