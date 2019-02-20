ceec839cf9c4898c0b0cc7c780ca6f07e217e941
Mohak <mohak.agarwal_ccv17@gla.ac.in>
2019-02-20 11:08:19 +0530

    add getter and setter for data

--- src/main/Node.java
+++ src/main/Node.java
@@ -1,4 +1,13 @@
 package main;
 
 public class Node {
+    private int data;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 }
